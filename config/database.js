caee80c4f99ed301f2aad2406feb08f6759ec763
Heloísa Real <85493260+heloreal17@users.noreply.github.com>
2022-03-21 13:19:45 -0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const conexao = async() => {
+    //conexao local
+    //var bdlocal = await mongoose.connect (1mongodb://localhost/AC1')
+    //conexao atlas
+    var atlas = await mongoose.connect('mongodb+srv://useradmin:heloreal17@cluster0.1k2zm.mongodb.net/test')
+}
+
+module.exports = conexao
